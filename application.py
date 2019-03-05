756f5a0b4e6c5b3ce8326a19db4cf8b00f149727
iguanaus4@yahoo.com <johnpeurifoy@yahoo.com>
2019-03-04 22:36:03 -0500

    Just changed characters to be working.

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ application = Flask(__name__)
 application.debug = True
 
 
-@application.route(‘/’, methods=[‘GET’])
+@application.route("/", methods=["GET"])
 def hello():
-	return ‘<p>Hello world</p>’
-if __name__ == “__main__”:
+	return "<p>Hello world</p>"
+if __name__ == "__main__":
 	application.run()
